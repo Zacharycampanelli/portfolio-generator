ab3822bdd2ea84e22f644fbfc0043ab9b61ab116
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-01-18 18:59:27 -0500

    added inquirer package

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const printProfileData = (profileDataArr) => {
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
+// const pageHTML = generatePage(name, github);
 
-  console.log("===============");
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
